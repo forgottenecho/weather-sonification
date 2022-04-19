19d14605289341a99aabd40d44f62a7e04d03cf6
forgottenecho <39808231+forgottenecho@users.noreply.github.com>
2022-04-18 23:03:43 -0400

    scale class works

--- midi_encoder.py
+++ midi_encoder.py
@@ -2,13 +2,55 @@ import pandas
 import matplotlib.pyplot as plt
 from midiutil import MIDIFile
 
-def step_down(prev_note: int) -> int:
-    pass
+class Scale():
 
-def step_up(prev_note: int) -> int:
-    pass
+    def __init__(self, root_note: int, scale_type: int):
 
-def weather_to_midi(data_path: str, output_path: str, root_note: int, scale: int) -> None:
+        # general processing vars
+        self._curr_note = root_note
+        self._scale_ptr = 0
+
+        # determine stepping pattern based on scale type
+        if scale_type == 0:
+            # major scale
+            self._steps = [2, 2, 1, 2, 2, 2, 1] # WWHWWWH
+        elif scale_type == 1:
+            # minor scale
+            self._steps = [] # FIXME add steps
+        else:
+            # error, scale code did not match any of ours
+            raise Exception('scale_type not recognized.')
+
+    # makes one step down the scale
+    def step_down(self):
+        # decrement the ptr
+        self._scale_ptr -= 1
+
+        # wrap around if necessary
+        if self._scale_ptr == -1:
+            self._scale_ptr = len(self._steps) - 1
+        
+        # subtract the value from the current note to find new note
+        self._curr_note -= self._steps[self._scale_ptr]
+
+
+    # makes one step up the scale
+    def step_up(self):
+        # add the value to the current note to find new note
+        self._curr_note += self._steps[self._scale_ptr]
+
+        # increment the ptr
+        self._scale_ptr += 1
+
+        # wrap around if necessary
+        if self._scale_ptr == len(self._steps):
+            self._scale_ptr = 0
+    
+    # getter for _curr_note
+    def get_note(self):
+        return self._curr_note
+
+def weather_to_midi(data_path: str, output_path: str, root_note: int, scale_type: int) -> None:
 
     # load up the data
     data = pandas.read_csv(data_path)
@@ -57,10 +99,12 @@ def weather_to_midi(data_path: str, output_path: str, root_note: int, scale: int
             note = last_note
         elif tdelta < 0:
             # temp went down, step down the scale
-            note = step_down(last_note)
+            scale.step_down()
+            note = scale.get_note()
         elif tdelta > 0:
             # temp went up, step up the scale
-            note = step_up(last_note)
+            scale.step_up()
+            note = scale.get_note()
 
         last_row = row
         last_note = note
@@ -68,4 +112,13 @@ def weather_to_midi(data_path: str, output_path: str, root_note: int, scale: int
     print('debug')
 
 if __name__ == '__main__':
+    # code for testing Scale class
+    scale = Scale(60, 0) # init obj
+    for i in range(10): # ten steps up the scale
+        scale.step_up()
+        print(scale.get_note())
+    for i in range(20): # 20 steps down the scale
+        scale.step_down()
+        print(scale.get_note())
+    
     weather_to_midi('nj_weather_data.csv', 'output.mid', 60, 0)
